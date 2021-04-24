1934ddf246c9ba0a277f7fd0048a7971322706f2
Ithamar R. Adema <ithamar@upgrade-android.com>
2021-04-24 09:48:59 +0200

    Fix loading issues
    - Only load map once
    - Show loading indicator
    - Add FPS stats

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
-import { OrbitControls } from '@react-three/drei'
+import { Stats, OrbitControls } from '@react-three/drei'
 import { Canvas, useThree } from '@react-three/fiber'
 
 import { loadArea } from '../legacy/hunt'
 
-function Terrain() {
+import './Home.css'
+
+interface Props {
+    setLoading: (isLoading: boolean) => void;
+}
+
+function Terrain({ setLoading }: Props) {
     const { scene } = useThree();
 
+    console.log('Terrain')
     React.useEffect(() => {
         console.time("loadArea");
         loadArea("AREA1").then(world => {
@@ -15,44 +22,51 @@ function Terrain() {
             if (world) {
                 scene.add(world)
             }
+            setLoading(false);
         })
-    })
+    }, [setLoading, scene])
     return null;
 }
 
 export default function Home() {
+    const [ isLoading, setLoading ] = React.useState(true);
+
     return (
-        <Canvas
-            shadows
-            linear
-            camera={{
-                position: [ 0, 12000, 12000 ],
-                fov: 30,
-                near: 1,
-                far: 1000000,
-            }}
-        >
-            <directionalLight
-                args={[0xdfebff, 1]}
-                position={[50, 200, 100]}
-                shadow-mapSize-width={1024}
-                shadow-mapSize-height={1024}
-                shadow-camera-left={-300}
-                shadow-camera-right={300}
-                shadow-camera-top={300}
-                shadow-camera-bottom={-300}
-                shadow-camera-far={1000}
-                castShadow
-            />
-            <ambientLight
-                args={[0x666666]}
-            />
+        <>
+            {isLoading && (<div className="center"><p>Loading, please wait</p></div>)}
+            <Canvas
+                shadows
+                linear
+                camera={{
+                    position: [ 0, 12000, 12000 ],
+                    fov: 30,
+                    near: 1,
+                    far: 1000000,
+                }}
+            >
+                <Stats />
+                <directionalLight
+                    args={[0xdfebff, 1]}
+                    position={[50, 200, 100]}
+                    shadow-mapSize-width={1024}
+                    shadow-mapSize-height={1024}
+                    shadow-camera-left={-300}
+                    shadow-camera-right={300}
+                    shadow-camera-top={300}
+                    shadow-camera-bottom={-300}
+                    shadow-camera-far={1000}
+                    castShadow
+                />
+                <ambientLight
+                    args={[0x666666]}
+                />
 
-            <OrbitControls
-                maxPolarAngle={0.4*Math.PI}
-            />
+                <OrbitControls
+                    maxPolarAngle={0.4*Math.PI}
+                />
 
-            <Terrain />
-        </Canvas>
+                <Terrain setLoading={setLoading} />
+            </Canvas>
+        </>
     )
 }
