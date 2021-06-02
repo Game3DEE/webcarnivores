0568acfbde3d0f47cb88a1d7d2da427147d11274
Ithamar R. Adema <ithamar@upgrade-android.com>
2021-06-02 09:26:12 +0200

    flagdump: Handle 2-bit flags, for both C1/C2
    Based on the flags as defined in https://www.tapatalk.com/groups/the_carnivores_saga/file-formats-f24/

--- src/tools/flagdump.js
+++ src/tools/flagdump.js
@@ -5,13 +5,12 @@ const MAP = require('../kaitai/carnivores_map');
 const KaitaiStream = require('kaitai-struct').KaitaiStream;
 const PNG = require('pngjs').PNG;
 
-
 const mapFname = process.argv[2];
 const baseName = path.basename(mapFname, '.MAP');
 
 const map = new MAP(new KaitaiStream(fs.readFileSync(mapFname)));
 
-function dumpFlagImage(bitVal, map, pngName, mapDim) {
+function dumpFlagImage(pngName, mapDim, getPixValue) {
     const png = new PNG({
         colorType: 0,
         width: mapDim,
@@ -21,7 +20,7 @@ function dumpFlagImage(bitVal, map, pngName, mapDim) {
     for (let y = 0; y < mapDim; y++) {
         for (let x = 0; x < mapDim; x++) {
             const i = y * mapDim + x;
-            const pix = (map[i] & bitVal) ? 0xff : 0;
+            const pix = getPixValue(i,x,y);
             png.data[i*4+0] = pix;
             png.data[i*4+1] = pix;
             png.data[i*4+2] = pix;
@@ -31,7 +30,43 @@ function dumpFlagImage(bitVal, map, pngName, mapDim) {
     fs.writeFileSync(pngName, PNG.sync.write(png));
 }
 
-const numBits = map.isV1 ? 8 : 16;
-for (let i = 0; i < numBits; i++) {
-    dumpFlagImage(1 << i, map.flagsMap, `${i}-${baseName}.png`, map.mapSize)
+// Dump bit 0/1 into 2bit color image (iso 1)
+// TDirection: texture direction
+dumpFlagImage(`${baseName}-B0-B1.png`, map.mapSize,
+    mIdx => ((map.flagsMap[mIdx] & 2) | (map.flagsMap[mIdx] & 1)) * (255/3)  );
+
+// If this is a Carnivores (1) map...
+if (map.isV1) {
+    // dump rest of the bits (all unknown, so single bits)
+    for (let i = 2; i < 8; i++) {
+        dumpFlagImage(`${baseName}-B${i}.png`, map.mapSize,
+            mIdx => (map.flagsMap[mIdx] & (1 << i)) ? 0xff : 0);
+    }
+} else {
+    // For Carnivores 2 & IceAge we have more 2bit color images to generate
+
+    // Bit 2/3: object rotation
+    dumpFlagImage(`${baseName}-B2-B3.png`, map.mapSize,
+        mIdx => {
+            const val = (map.flagsMap[mIdx] >> 2) & 3;
+            return val * (255/3);
+        }
+    );
+
+    // Bit 8/9: object rotation
+    dumpFlagImage(`${baseName}-B8-B9.png`, map.mapSize,
+        mIdx => {
+            const val = (map.flagsMap[mIdx] >> 2) & 3;
+            return val * (255/3);
+        }
+    );
+
+    // Now dump single bits
+    for (let i = 4; i < 15; i++) {
+        // skip our bit 8/9 combo, as we already dumped them
+        if (i == 8 || i == 9) continue;
+
+        dumpFlagImage(`${baseName}-B${i}.png`, map.mapSize,
+            mIdx => (map.flagsMap[mIdx] & (1 << i)) ? 0xff : 0);
+    }
 }
