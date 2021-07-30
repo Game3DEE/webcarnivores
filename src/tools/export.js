1d49f60c35e6390d294cf298501bb5e899aa7570
Ithamar R. Adema <ithamar@upgrade-android.com>
2021-07-30 19:20:53 +0200

    tools: PRJ file handling in export
    Simple test to see if same-named textures are
    actually the same textures across PRJ files.
    
    Result: NO :(

--- src/tools/export.js
+++ src/tools/export.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const md5 = require('md5');
+
 const KaitaiStream = require('kaitai-struct').KaitaiStream;
 const PNG = require('pngjs').PNG;
 
@@ -10,9 +12,67 @@ const RSC = require('../kaitai/Rsc');
 const mapFname = process.argv[2];
 const baseName = path.basename(mapFname, '.MAP');
 const rscFname = mapFname.replace(/MAP$/, 'RSC');
+const prjFname = mapFname.replace(/MAP$/, 'PRJ');
+
+const filenames = JSON.parse(fs.readFileSync(path.join(__dirname, 'filenames.json'), 'utf8'));
 
 const map = new MAP(new KaitaiStream(fs.readFileSync(mapFname)));
 const rsc = new RSC(new KaitaiStream(fs.readFileSync(rscFname)), undefined, undefined, map.version);
+const prj = fs.readFileSync(prjFname, 'utf8');
+
+const textureNames = [];
+const objectNames = [];
+const randomNames = [];
+const ambientNames = [];
+
+function addFilename(name, data, type) {
+    const chksum = md5(data);
+    const idx = filenames[type].findIndex(file => file.name.toLowerCase() == name.toLowerCase());
+
+    if (idx == -1) {
+        filenames[type].push({ name, md5: chksum, count: 1 });
+    } else {
+        if (filenames[type][idx].md5 != chksum) {
+            console.log(`${name}: md5 mismatch: ${filenames[type][idx].md5} != ${chksum}`)
+        } else {
+            filenames[type][idx].count++;
+        }
+    }
+}
+
+function parsePRJ() {
+    prj.split('\n').forEach(line => {
+        const l = line.trim();
+        if (!l) return; // skip empty lines
+        const t = l.match(/^Texture([0-9]+)=(.+$)/i);
+        const o = l.match(/^Object([0-9]+)=(.+$)/i);
+        const r = l.match(/^Random([0-9]+)=(.+$)/i);
+        const a = l.match(/^Ambient([0-9]+)=(.+$)/i);
+        if (t) {
+            const index = parseInt(t[1], 10) -1;
+            textureNames[index] = t[2];
+            addFilename(t[2], rsc.textures[index].data, 'texture')
+            //fs.writeFileSync(`${baseName}-tex-${t[2]}.raw`, rsc.textures[index].data);
+        } else if (o) {
+            const index = parseInt(o[1], 10) -1;
+            //objectNames[index] = o[2];
+        } else if (r) {
+            const index = parseInt(r[1], 10) -1;
+            randomNames[index] = r[2];
+            //fs.writeFileSync(`${baseName}-random-${r[2]}.raw`, rsc.randomSounds[index].pcm);
+            addFilename(r[2], rsc.randomSounds[index].pcm, 'random')
+        } else if (a) {
+            const index = parseInt(a[1], 10) -1;
+            ambientNames[index] = a[2];
+            addFilename(a[2], rsc.ambientSounds[index].pcm, 'ambient')
+            //fs.writeFileSync(`${baseName}-ambient-${a[2]}.raw`, rsc.ambientSounds[index].pcm);
+        } else {
+            console.log(`Ignoring line '${l}'`);
+        }
+    })
+
+    //console.log(filenames);
+}
 
 function exportTexture(texData, texName, width, height) {
     const png = new PNG({
@@ -55,15 +115,7 @@ function exportMap(map, pngName, mapDim) {
     }
     fs.writeFileSync(pngName, PNG.sync.write(png));
 }
-
-function exportModel(model, modelBaseName) {
-    let tree = {
-        position: [0,0,0],
-        children: [],
-    };
-    console.log(modelBaseName, model.nodes)
-}
-
+/*
 exportMap(map.heightMap, `${baseName}-hm.png`, map.mapSize);
 exportMap(map.waterMap, `${baseName}-wm.png`, map.mapSize);
 exportMap(map.dayLightMap, `${baseName}-day-lm.png`, map.mapSize);
@@ -78,7 +130,7 @@ if (map.version == 2) {
     exportTexture(rsc.dawnSkyTexture, `${baseName}-dawn-sky.png`, 256, 256);
     exportTexture(rsc.nightSkyTexture, `${baseName}-night-sky.png`, 256, 256);
 }
-rsc.models.forEach((model, modelIdx) => {
-    //exportModel(model, `model-${modelIdx}`);
-})
+*/
 
+parsePRJ();
+fs.writeFileSync(path.join(__dirname, 'filenames.json'), JSON.stringify(filenames, undefined, 2));
